da6e8008b3fe008f063421e41745fbbf311b77b6
boram065 <boram4784@gmail.com>
2024-04-07 02:19:01 +0900

    init

---
+++ src/components/splash/splash.js
@@ -0,0 +1,11 @@
+import splashStyle from '../../styles/splash/splash.module.css';
+
+function Splash() {
+    return (
+        <>
+            <div></div>
+        </>
+    )
+}
+
+export default Splash;
