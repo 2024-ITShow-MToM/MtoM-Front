0ee6ce86542fa54bbad8ce98e23d8fca0e174b19
boram065 <boram4784@gmail.com>
2024-04-07 19:01:54 +0900

    [feat] 새로운 기능 추가

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Splash from './pages/splash';
+import Login from './pages/login';
 
 function App() {
   return(
       <Router>
           <Routes>
               <Route path="/" element={<Splash />}/>
+              <Route path="/signin" element={<Login />}/>
           </Routes>
       </Router>
   )
--- src/pages/splash.js
+++ src/pages/splash.js
@@ -1,6 +1,16 @@
+import React, { useEffect } from 'react';
+
 import Splash from "../components/splash/splash";
 
 function SplashPage() {
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            window.location.href = '/signin';
+        }, 3000);
+
+        return () => clearTimeout(timer);
+    }, []);
+
     return (
         <>
             <Splash />
