b8ffd5f7c71e84d1962ad5d15f78439b0c609543
boram065 <boram4784@gmail.com>
2024-04-14 17:43:45 +0900

    feat: 이미지 삽입 기능 구현

---
+++ src/components/q&a/ImageBox.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+
+import '../../styles/common/Style.css';
+import styles from '../../styles/q&a/ImageBox.module.css';
+
+import { GoPlus } from "react-icons/go";
+
+function ImageBox() {
+    const [backgroundImage, setBackgroundImage] = useState(null);
+
+    const handleImageUpload = () => {
+        const input = document.createElement('input');
+        input.type = 'file';
+        input.accept = 'image/*';
+        input.onchange = (event) => {
+            const selectedFile = event.target.files[0];
+            const objectURL = URL.createObjectURL(selectedFile);
+            setBackgroundImage(objectURL);
+        };
+        input.click();
+    };
+
+    return (
+        <>
+            <div className={styles['container']}>
+                <div className={styles['background']} style={{backgroundImage: `url(${backgroundImage})`}} onClick={handleImageUpload}>
+                    {!backgroundImage && (
+                        <label htmlFor="imageInput" className={styles['circle']}>
+                            <GoPlus />
+                        </label>
+                    )}
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default ImageBox;
