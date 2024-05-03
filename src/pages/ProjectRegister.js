fde8909a5c7afd2e8c3bb8b26f387bcd5e32f8ed
boram065 <boram4784@gmail.com>
2024-05-04 02:17:32 +0900

    Feat: 프로젝트 등록 화면 구현

---
+++ src/pages/ProjectRegister.js
@@ -0,0 +1,11 @@
+import ProjectRegister from "../components/project/register/ProjectRegister";
+
+function ProjectRegisterPage() {
+    return (
+        <>
+            <ProjectRegister />
+        </>
+    )
+}
+
+export default ProjectRegisterPage;
