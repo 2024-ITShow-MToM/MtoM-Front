85df2595ee1a1384300862504bb0922bbcaeb826
boram065 <boram4784@gmail.com>
2024-05-03 23:46:03 +0900

    Feat: YourChat 프로필 구현

--- src/components/chat/group chat/YourChat.js
+++ src/components/chat/group chat/YourChat.js
@@ -5,8 +5,16 @@ function YourChat({ text, time }) {
     return (
         <>
             <div className={styles['container']}>
-                <div className={styles['your-chat']}>{text}</div>
-                <p>{time}</p>
+                <div className={styles['profileDiv']}> 
+                    <div className={styles['imgDiv']}> 
+                        <img src='/images/example.png' />
+                    </div> 
+                    <p>3413 최보람</p>
+                </div>
+                <div className={styles['content']}>
+                    <div className={styles['your-chat']}>{text}</div>
+                    <p>{time}</p>
+                </div>
             </div>
         </>
     )
