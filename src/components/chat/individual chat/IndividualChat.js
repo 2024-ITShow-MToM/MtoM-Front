835afc4c768959a25700f8a676f40d78b53344f2
boram065 <boram4784@gmail.com>
2024-05-03 22:47:16 +0900

    Feat: 개인 채팅 화면 구현

---
+++ src/components/chat/individual chat/IndividualChat.js
@@ -0,0 +1,20 @@
+import '../../../styles/common/Style.css';
+import styles from '../../../styles/chat/individual chat/IndividualChat.module.css';
+
+import Header from './Header';
+import ChattingBox from './ChattingBox';
+import SendInput from './SendInput';
+
+function IndividualChat() {
+    return (
+        <>
+            <Header name='3413 최보람'/>
+            <div className={styles['container']}>
+                <ChattingBox date='오늘' />
+                <SendInput />
+            </div>
+        </>
+    )
+}
+
+export default IndividualChat;
---
+++ src/pages/IndividualChat.js
@@ -0,0 +1,11 @@
+import IndividualChat from "../components/chat/individual chat/IndividualChat";
+
+function IndividualChatPage() {
+    return (
+        <>
+            <IndividualChat />
+        </>
+    )
+}
+
+export default IndividualChatPage;
