3598fef699fbd891adb022ffeb299066d168a82c
boram065 <boram4784@gmail.com>
2024-05-04 13:25:15 +0900

    Style: 버튼 위치 수정

--- src/components/project/register/ProjectRegister.js
+++ src/components/project/register/ProjectRegister.js
@@ -9,6 +9,7 @@ import { FiPlus } from 'react-icons/fi';
 import { FaRegCalendarAlt } from "react-icons/fa";
 
 import Header from '../Header';
+import { Link } from 'react-router-dom';
 
 function ProjectRegister() {
     const [backgroundImage, setBackgroundImage] = useState(null);
@@ -52,73 +53,77 @@ function ProjectRegister() {
         <>
             <Header title='프로젝트 등록'/>
             <div className={styles['container']}>
-                <div className={styles['content']}>
-                    {/* 이미지 선택 */}
-                    <div>
-                        <div className={styles['imgDiv']} onClick={handleImageUpload}>
-                            { backgroundImage ?
-                                <img src={`${backgroundImage}`} /> : <Icon icon='mdi:camera' />
-                            }
-                        </div>
-                    </div>
-
-                    {/* 제목 설명 입력 */}
-                    <div className={styles['titleAndcontext']}>
-                        <input type='text' placeholder='제목' />
-                        <input type='text' placeholder='설명 입력' />
-                    </div>
-                </div>
-
-                {/* 기간 설정 */}
-                <div className={styles['period']}>
-                    <div className={styles['recruitment']}>
-                        <p>모집 기간 설정</p>
-                        <div className={styles['setting']}>
-                            <div className={styles['settingBox']}></div>
-                            ~
-                            <div className={styles['settingBox']}></div>
-                            <FaRegCalendarAlt style={{ fontSize: '24px', color: '#FF6524' }} />
-                        </div>
-                    </div>
-                    
-                    <div className={styles['work']}>
-                        <p>작업 기간 설정</p>
-                        <div className={styles['setting']}>
-                            <div className={styles['settingBox']}></div>
-                            ~
-                            <div className={styles['settingBox']}></div>
-                            <FaRegCalendarAlt style={{ fontSize: '24px', color: '#FF6524' }} />
-                        </div>
-                    </div>
-                </div>
-
-                {/* 모집 인원 설정 */}
-                <div className={styles['people']}>
-                    <div className={styles['peopleTitle']}>
-                        <p>모집 인원</p>
-                        <p>{peopleNumber}명</p>
-                    </div>
-                    <div className={styles['peopleNum']}>
-                        {people.map(person => (
-                            <div className={styles['peopleDiv']} key={person.id}>
-                                <div className={styles['person']}>
-                                    <div className={styles['textDiv']}>{person.text}</div>
-                                        <div className={styles['peopleNumber']}>
-                                            {[...Array(person.id === 1 ? frontendCount : person.id === 2 ? designCount : person.id === 3 ? backendCount : planCount)].map((_, index) => (
-                                                <IoPersonSharp key={index+1} style={{ fontSize: '20px', color: '#5A5A5A' }} />
-                                            ))}
-                                        </div>
-                                </div>
-                                <FiPlus style={{ fontSize: '20px', color: '#5A5A5A', [person.id % 2 !== 0 ? 'right' : null]: '5%'  }} onClick={() => addPersonShape(person.id)}/>
+                <div>
+                    <div className={styles['content']}>
+                        {/* 이미지 선택 */}
+                        <div>
+                            <div className={styles['imgDiv']} onClick={handleImageUpload}>
+                                { backgroundImage ?
+                                    <img src={`${backgroundImage}`} /> : <Icon icon='mdi:camera' />
+                                }
                             </div>
-                        ))}
-                    </div>
-                </div>
+                        </div>
 
-                {/* 프로젝트 소개 */}
-                <div className={styles['projectText']}>
-                    <p>프로젝트 소개</p>
-                    <textarea />
+                        {/* 제목 설명 입력 */}
+                        <div className={styles['titleAndcontext']}>
+                            <input type='text' placeholder='제목' />
+                            <input type='text' placeholder='설명 입력' />
+                        </div>
+                    </div>
+
+                    {/* 기간 설정 */}
+                    <div className={styles['period']}>
+                        <div className={styles['recruitment']}>
+                            <p>모집 기간 설정</p>
+                            <div className={styles['setting']}>
+                                <div className={styles['settingBox']}></div>
+                                ~
+                                <div className={styles['settingBox']}></div>
+                                <Link to='/project/recruitment-period' style={{ textDecoration: 'none', color: 'black' }}>
+                                    <FaRegCalendarAlt style={{ fontSize: '24px', color: '#FF6524' }} />
+                                </Link>
+                            </div>
+                        </div>
+                        
+                        <div className={styles['work']}>
+                            <p>작업 기간 설정</p>
+                            <div className={styles['setting']}>
+                                <div className={styles['settingBox']}></div>
+                                ~
+                                <div className={styles['settingBox']}></div>
+                                <FaRegCalendarAlt style={{ fontSize: '24px', color: '#FF6524' }} />
+                            </div>
+                        </div>
+                    </div>
+
+                    {/* 모집 인원 설정 */}
+                    <div className={styles['people']}>
+                        <div className={styles['peopleTitle']}>
+                            <p>모집 인원</p>
+                            <p>{peopleNumber}명</p>
+                        </div>
+                        <div className={styles['peopleNum']}>
+                            {people.map(person => (
+                                <div className={styles['peopleDiv']} key={person.id}>
+                                    <div className={styles['person']}>
+                                        <div className={styles['textDiv']}>{person.text}</div>
+                                            <div className={styles['peopleNumber']}>
+                                                {[...Array(person.id === 1 ? frontendCount : person.id === 2 ? designCount : person.id === 3 ? backendCount : planCount)].map((_, index) => (
+                                                    <IoPersonSharp key={index+1} style={{ fontSize: '20px', color: '#5A5A5A' }} />
+                                                ))}
+                                            </div>
+                                    </div>
+                                    <FiPlus style={{ fontSize: '20px', color: '#5A5A5A', [person.id % 2 !== 0 ? 'right' : null]: '5%'  }} onClick={() => addPersonShape(person.id)}/>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+
+                    {/* 프로젝트 소개 */}
+                    <div className={styles['projectText']}>
+                        <p>프로젝트 소개</p>
+                        <textarea />
+                    </div>
                 </div>
 
                 <button>프로젝트 만들기</button>
