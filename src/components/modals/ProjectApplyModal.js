fb51e7f130827a4df519b5d3756c1ed16b6e2ad7
boram065 <boram4784@gmail.com>
2024-05-05 16:16:52 +0900

    Feat: 모달 구현

---
+++ src/components/modals/ProjectApplyModal.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Modal from 'react-modal';
+
+import '../../styles/common/Style.css';
+import styles from '../../styles/modals/ProjectApplyModal.module.css';
+
+import { FaCircleCheck } from "react-icons/fa6";
+
+function ProjectApplyModal() {
+    return (
+        <Modal isOpen={true} className={styles['modal']}>
+            <div className={styles['container']}>
+                <FaCircleCheck style={{ fontSize: '30px', color: '#FF6524' }}/>
+                <p>프로젝트 신청 완료!</p>
+                <p>프로젝트 채팅방 초대 완료</p>
+            </div>
+        </Modal>
+    )
+}
+
+export default ProjectApplyModal;
