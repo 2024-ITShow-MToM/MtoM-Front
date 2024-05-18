7501b00be145968021da8cf00e19ce74d0510f7e
boram065 <boram4784@gmail.com>
2024-05-18 20:14:26 +0900

    Fix: 이미지 다시 못넣음 해결

--- src/components/q&a/write/ImageBox.js
+++ src/components/q&a/write/ImageBox.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 
 import '../../../styles/common/Style.css';
 import styles from '../../../styles/q&a/write/ImageBox.module.css';
@@ -7,6 +7,7 @@ import { GoPlus } from "react-icons/go";
 
 function ImageBox({ setImg }) {
     const [backgroundImage, setBackgroundImage] = useState(null);
+    const inputRef = useRef(null);
 
     const handleImageSelect = (event) => {
         const selectedFile = event.target.files[0];
@@ -15,17 +16,21 @@ function ImageBox({ setImg }) {
         setImg(selectedFile);
     };
 
+    const handleClickImage = () => {
+        inputRef.current.click();
+    };
+
     return (
         <>
             <div className={styles['container']}>
-                <div className={styles['background']} style={{backgroundImage: `url(${backgroundImage})`}}>
+                <div className={styles['background']} style={{backgroundImage: `url(${backgroundImage})`}} onClick={handleClickImage}>
                     {!backgroundImage && (
                         <label htmlFor="imageInput" className={styles['circle']}>
                             <GoPlus />
                         </label>
                     )}
                 </div>
-                <input type="file" id="imageInput" accept="image/*" onChange={handleImageSelect} style={{display: 'none'}} />
+                <input ref={inputRef} type="file" id="imageInput" accept="image/*" onChange={handleImageSelect} style={{display: 'none'}} />
             </div>
         </>
     )
