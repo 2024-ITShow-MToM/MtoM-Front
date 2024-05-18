d4b562d16bb1687038983285d73c2dfa14b2e263
boram065 <boram4784@gmail.com>
2024-05-18 11:48:00 +0900

    Feat: Q&A 게시물 생성 데이터 넘기기

--- src/components/q&a/write/Q&APostWrite.js
+++ src/components/q&a/write/Q&APostWrite.js
@@ -1,14 +1,32 @@
+import React, { useState } from 'react';
+
 import '../../../styles/common/Style.css';
 import styles from '../../../styles/q&a/write/Q&APostWrite.module.css';
 import ImageBox from './ImageBox';
 import QandAPostWriteInput from './Q&APostWriteInput';
 
 function QandAPostWrite() {
+    const [tags, setTags] = useState([]);
+    const userId = localStorage.getItem("userId"); // userId는 회원가입했을 때 아이디
+    const [postData, setPostData] = useState({
+        img: null,
+        title: null,
+        content: null,
+        hashtags: tags
+    });
+
+    const register = () => {
+        console.log(postData);
+    }
+
     return (
         <>
             <div className={styles['container']}>
-                <ImageBox />
-                <QandAPostWriteInput />
+                <ImageBox setPostData={setPostData} />
+                <QandAPostWriteInput setPostData={setPostData} tags={tags} setTags={setTags} />
+                <div className={styles['button']}>
+                    <button onClick={register}>등록하기</button>
+                </div>
             </div>
         </>
     )
--- src/components/q&a/write/Q&APostWriteInput.js
+++ src/components/q&a/write/Q&APostWriteInput.js
@@ -1,56 +1,73 @@
-import React, { useState, useRef } from 'react';
+import React, { useRef } from 'react';
 
 import '../../../styles/common/Style.css';
 import styles from '../../../styles/q&a/write/Q&APostWriteInput.module.css';
 
-function QandAPostWriteInput() {
-    const [tags, setTags] = useState([]);
+import { TiDeleteOutline } from "react-icons/ti";
+
+function QandAPostWriteInput({ setPostData, tags, setTags }) {
+    const handleChange = (e) => {
+        const { id, value } = e.target;
+        setPostData(data => ({
+            ...data,
+            [id]: value
+        }));
+    };
+
     const handleKeyPress = (event) => {
         if (event.key === 'Enter') {
             const tagValue = event.target.value.trim();
             if (tagValue !== '') {
-                setTags([...tags, tagValue]);
+                const updatedTags = [...tags, tagValue];
+                const tagsString = updatedTags.join(', ');
+                setTags(updatedTags);
+                setPostData(prevData => ({
+                    ...prevData,
+                    hashtags: tagsString
+                }));
                 event.target.value = '';
             }
         }
     };
 
+    const handleTagDelete = (index) => {
+        const updatedTags = tags.filter((_, i) => i !== index);
+        const tagsString = updatedTags.join(', ');
+        setTags(updatedTags);
+        setPostData(prevData => ({
+            ...prevData,
+            hashtags: tagsString
+        }));
+    };
+    
     const textareaRef = useRef(null);
     const handleTextareaInput = () => {
         const textarea = textareaRef.current;
         textarea.style.height = 'auto';
         textarea.style.height = `${textarea.scrollHeight}px`;
-        setContent(textarea.value);
+        setPostData(prevData => ({
+            ...prevData,
+            'content': textarea.value
+        }));
     };
-
-    const [title, setTitle] = useState('');
-    const [content, setContent] = useState('');
-    const handleTitleChange = (event) => {
-        setTitle(event.target.value);
-    };
-
-    const [valueArr, setValueArr] = useState([]);
-    const arr = () => {
-        const newArr = [...valueArr, { title, content, tags }];
-        setValueArr(newArr); // title, content, tags 배열에 저장
-    }
 
     return (
         <>
             <div className={styles['container']}>
                 <div className={styles['questionInput']}>
                     <input 
+                        id='title'
                         placeholder='질문 제목' 
                         type='text'
-                        value={title}
-                        onChange={handleTitleChange}
+                        onChange={handleChange}
                     />
                 </div>
 
                 <div className={styles['textarea']}>
                     <textarea 
+                        id='content'
                         placeholder='내용 입력하기' 
-                        type='text' 
+                        type='text'
                         ref={textareaRef}
                         onInput={handleTextareaInput}
                     />
@@ -60,15 +77,12 @@ function QandAPostWriteInput() {
                     <input placeholder='태그 입력' onKeyPress={handleKeyPress} type='text'/>
                     <div className={styles['tagContainer']}>
                         {tags.map((tag, index) => (
-                            <div className={styles['tag']} key={index}>
+                            <div className={styles['tag']} key={index} onClick={() => handleTagDelete(index)}>
                                 <p>{tag}</p>
+                                <TiDeleteOutline />
                             </div>
                         ))}
                     </div>
-                </div>
-
-                <div className={styles['button']}>
-                    <button onClick={arr}>등록하기</button>
                 </div>
             </div>
         </>
