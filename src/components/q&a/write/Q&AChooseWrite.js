decce97f04363bc96205ad37403a22815c9c9e91
boram065 <boram4784@gmail.com>
2024-05-18 12:01:29 +0900

    Feat: 양자택일 생성 데이터 전달하기

--- src/components/q&a/write/Q&AChooseWrite.js
+++ src/components/q&a/write/Q&AChooseWrite.js
@@ -8,21 +8,36 @@ import QandAChooseWriteInput from './Q&AChooseWriteInput';
 function QandAChooseWrite() {
     const [isPlaceholderHidden, setPlaceholderHidden] = useState(false);
     const inputRef = useRef(null);
+    const [chooseData, setChooseData] = useState({
+        title: null,
+        option1: null,
+        option2: null
+    });
 
+    const register = () => {
+        console.log(chooseData);
+    }
+
+    const handleChange = (e) => {
+        const { id, value } = e.target;
+        setChooseData(data => ({
+            ...data,
+            [id]: value
+        }));
+    };
+
+    // 제목 타이틀 다른 곳 클릭하면 보이게
     useEffect(() => {
         const handleOutsideClick = (event) => {
             if (inputRef.current && !inputRef.current.contains(event.target)) {
                 setPlaceholderHidden(false);
             }
         };
-
         document.addEventListener('mousedown', handleOutsideClick);
-
         return () => {
             document.removeEventListener('mousedown', handleOutsideClick);
         };
     }, []);
-
     const handleInputFocus = () => {
         setPlaceholderHidden(true);
     };
@@ -31,7 +46,9 @@ function QandAChooseWrite() {
         <>
             <div className={styles['container']}>
                 <div className={styles['title']}>
-                    <input 
+                    <input
+                        id='title'
+                        onChange={handleChange}
                         ref={inputRef}
                         placeholder={isPlaceholderHidden ? '' : '제목'} 
                         onFocus={handleInputFocus} 
@@ -39,7 +56,10 @@ function QandAChooseWrite() {
                     <hr />
                 </div>
 
-                <QandAChooseWriteInput />
+                <QandAChooseWriteInput setChooseData={setChooseData} />
+                <div className={styles['button']}>
+                    <button onClick={register}>등록하기</button>
+                </div>
             </div>
         </>
     )
