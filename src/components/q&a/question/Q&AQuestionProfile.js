6012a1b764d7e46c7dabb5b480d9083281a25ce7
boram065 <boram4784@gmail.com>
2024-05-19 17:42:32 +0900

    Feat: 게시글 상세페이지 작성자 프로필 서버 연결

--- src/components/q&a/question/Q&AQuestionProfile.js
+++ src/components/q&a/question/Q&AQuestionProfile.js
@@ -1,38 +1,28 @@
-import React, { useEffect, useState } from 'react';
-import { HOST } from '../../../config/Config';
-import axios from 'axios';
 import '../../../styles/common/Style.css';
 import styles from '../../../styles/q&a/question/Q&AQuestionProfile.module.css';
 
-function QandAQuestionProfile() {
-    // const userId = localStorage.getItem("userId");
-    // const[data, setData] = useState([]);
-    // useEffect(() => {
-    //     async function fetchData() {
-    //         try {
-    //             const response = await axios.get(`${HOST}/api/users`);
-    //             if (response.status === 200) {
-    //                 console.log("회원 조회 성공");
-    //                 setData(response.data);
-    //             } else {
-    //                 console.log("회원 조회 실패", response.status);
-    //             }
-    //         } catch(error) {
-    //             console.log("서버 연결 실패", error);
-    //         }
-    //     }
-    //     fetchData();
-    // }, []);
+function QandAQuestionProfile({ data }) {
+    if (!data) {
+        return;
+    }
 
     return (
         <>
-            <div className={styles['container']}>
-                <div className={styles['imgDiv']}> <img src='/images/example.png' /> </div>
-                <div className={styles['info']}>
-                    <p>디자인과</p>
-                    <p>3413 최보람</p>
-                </div>
-            </div>
+            {
+                data.map((item, index) => {
+                    return (
+                        <>
+                            <div className={styles['container']} key={index}>
+                                <div className={styles['imgDiv']}> <img src={item.profile} /> </div>
+                                <div className={styles['info']}>
+                                    <p>{item.major}</p>
+                                    <p>{item.name}</p>
+                                </div>
+                            </div>
+                        </>
+                    )
+                })
+            }
         </>
     )
 }
