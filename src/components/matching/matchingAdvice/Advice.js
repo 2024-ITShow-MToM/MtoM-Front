bd512e69e9f89c349a73cbf03de2ba1e9fdd9e1b
해원 <haewon1106@gmail.com>
2024-05-24 10:35:27 +0900

    Feat: 매칭화면 조언 텍스트 컴포넌트 구현

---
+++ src/components/matching/matchingAdvice/Advice.js
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import styles from "../../../styles/matching/Advice.module.css"
+
+function Advice(props) {
+    const { text, onClick } = props;
+    const [isSelected, setIsSelected] = useState(false);
+
+    const handleClick = () => {
+        setIsSelected(!isSelected);
+        onClick();
+    };
+
+    return (
+        <div
+            className={`${styles['advice-box']} ${isSelected ? styles['selected'] : ''}`}
+            onClick={handleClick}>
+            {text}
+        </div>
+    );
+}
+
+export default Advice;
