ec04a43341e99f8afa6a4250c270bf5a1b8013ab
해원 <haewon1106@gmail.com>
2024-05-28 09:31:10 +0900

    Feat: 더보기 및 태그 추가

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,6 +7,8 @@ import Mento from "../components/home/Mento";
 import Text from "../components/home/Text";
 import Interview from "../components/home/Interview";
 import MentoList from "../components/home/MentoList";
+import ChooseItem from "../components/home/interview/ChooseItem";
+import ChooseContainer from "../components/home/interview/ChooseContainer";
 
 function Home() {
     return (
@@ -17,13 +19,19 @@ function Home() {
                 <Text text="김미림님에게 추천하는 멘토선배"/>
                 <div className={styles['mento-container']}>
                     <Major />
+                </div>
+            </div>
                     <div>
                         <MentoList />
                     </div>
-                </div>
+            <div className={styles['home-container']}>
                 <div className={styles['interview-container']}>
                     <p className={styles['interview-text']}>interview</p>
-                    <Text text="졸업후 선배님들의 인터뷰"/>
+                    <div className={styles['interview-more']}>
+                        <Text text="졸업후 선배님들의 인터뷰"/>
+                        <div>더보기</div>
+                    </div>
+                    <ChooseContainer tagname="HOT"/>
                     <div className={styles['interview-box']}>
                         <Interview text="미림에서 꿈을 이뤘어요" info="6기 박혜림 선배님의 미림생활 부터 회사생활까지 지금 만나보세요!"/>
                         <Interview text="미림에서 꿈을 이뤘어요" info="6기 박혜림 선배님의 미림생활 부터 회사생활까지 지금 만나보세요!"/>
