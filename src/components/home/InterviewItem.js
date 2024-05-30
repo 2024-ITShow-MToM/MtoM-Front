9c3b81e1b7ff4831eeb8a4b45299a4941b001e98
해원 <haewon1106@gmail.com>
2024-05-30 20:30:41 +0900

    Docs: interviewItem 파일명 변경

--- src/components/home/InterviewItem.js
+++ src/components/home/InterviewItem.js
@@ -1,19 +1,25 @@
 import React from "react";
 import styles from "../../styles/home/Interview.module.css"
 import { Icon } from "@iconify/react";
+import { useNavigate } from 'react-router-dom';
 
-function Interview(props) {
+function InterviewItem(props) {
     const {text, info} = props;
+
+    const navigate = useNavigate();
+    const handleNext = () => {
+        navigate('/interviewContent');
+      };
     return(
         <div className={styles['interview-container']}>
             <img src="/images/HomeImg.png" alt="인터뷰 이미지" className={styles['interview-img']}/>
             <p className={styles['text']}>{text}</p>
             <p className={styles['info']}>{info}</p>
-            <div className={styles["read-more-container"]}>
+            <div className={styles["read-more-container"]} onClick={handleNext}>
                 <p>자세히 보기</p>
                 <Icon icon="uiw:swap-right" />
             </div>
         </div>
     )
 }
-export default Interview
+export default InterviewItem;
