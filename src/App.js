35900901b2689585c9e86a025f3f17c6b101454d
해원 <haewon1106@gmail.com>
2024-06-08 16:43:27 +0900

    Docs: 페이지 추가

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import ProjectProvider from './components/project/register/ProjectProvider';
 
 import Splash from './pages/Splash';
 import Login from './pages/Login';
@@ -23,9 +22,11 @@ import Chatting from './pages/Chatting';
 import IndividualChat from './pages/IndividualChat';
 import GroupChat from './pages/GroupChat';
 
+import Project from './pages/Project'
 import ProjectRegister from './pages/ProjectRegister';
 import RecruitmentPeriod from './pages/RecruitmentPeriod';
 import WorkPeriod from './pages/WorkPeriod';
+import ProjectApplication from './components/project/ProjectApplication'
 
 import MatchingField from './pages/MatchingField'
 import MatchingMajor from './pages/MatchingMajor'
@@ -42,49 +43,49 @@ import ProjectDetail from './components/project/detail/ProjectDetail';
 
 function App() {
     return(
-      <ProjectProvider>
-        <Router>
-            <Routes>
-                <Route path="/" element={<Splash />}/>
-                <Route path="/signin" element={<Login />}/>
-                <Route path="/signup" element={<Join />}/>
+      <Router>
+          <Routes>
+              <Route path="/" element={<Splash />}/>
+              <Route path="/signin" element={<Login />}/>
+              <Route path="/signup" element={<Join />}/>
 
-                <Route path="/home" element={<Home />}/>
+              <Route path="/home" element={<Home />}/>
 
-                <Route path='/alarm' element={<Alarm />}/>
+              <Route path='/alarm' element={<Alarm />}/>
 
-                <Route path='/interview' element={<Interview />}/>
-                <Route path='/interviewContent' element={<InterviewContent />}/>
+              <Route path='/interview' element={<Interview />}/>
+              <Route path='/interviewContent' element={<InterviewContent />}/>
 
-                <Route path='/profile/register' element={<ProfileRegister />} />
-                
-                <Route path='/q&a' element={<QandA />} />
-                <Route path='/q&a/write' element={<QandAWrite />} />
-                <Route path='/q&a/question/:id' element={<QandAQuestion />} />
+              <Route path='/profile/register' element={<ProfileRegister />} />
+              
+              <Route path='/q&a' element={<QandA />} />
+              <Route path='/q&a/write' element={<QandAWrite />} />
+              <Route path='/q&a/question/:id' element={<QandAQuestion />} />
 
-                <Route path='/chat' element={<Chatting/>} />
-                <Route path='/chat/individual/:username' element={<IndividualChat />} />
-                <Route path='/chat/group' element={<GroupChat />} />
+              <Route path='/chat' element={<Chatting/>} />
+              <Route path='/chat/individual/:username' element={<IndividualChat />} />
+              <Route path='/chat/group' element={<GroupChat />} />
 
-                <Route path='/project/register' element={<ProjectRegister />} />
-                <Route path='/project/recruitment-period' element={<RecruitmentPeriod />} />
-                <Route path='/project/work-period' element={<WorkPeriod />} />
-                <Route path='/project/detail/:id' element={<ProjectDetail />}/>
+              <Route path='/project' element={<Project />} />
+              <Route path='/project/register' element={<ProjectRegister />} />
+              <Route path='/project/recruitment-period' element={<RecruitmentPeriod />} />
+              <Route path='/project/work-period' element={<WorkPeriod />} />
+              <Route path='/project/detail/:id' element={<ProjectDetail />}/>
+              <Route path='/project/projectApplication' element={<ProjectApplication />}/>
 
-                <Route path='/mento/register' element={<MentoRegister />}/>
+              <Route path='/mento/register' element={<MentoRegister />}/>
 
-                <Route path='/matchingField' element={<MatchingField />}/>
-                <Route path='/matchingMajor' element={<MatchingMajor />}/>
-                <Route path='/matchingAdvice' element={<MatchingAdvice />}/>
-                <Route path='/matchingMento' element={<MatchingMento />}/>
-                <Route path='/matchingEnd' element={<MatchingEnd />}/>
-                <Route path='/matchingApplication' element={<MatchingApplication />}/>
+              <Route path='/matchingField' element={<MatchingField />}/>
+              <Route path='/matchingMajor' element={<MatchingMajor />}/>
+              <Route path='/matchingAdvice' element={<MatchingAdvice />}/>
+              <Route path='/matchingMento' element={<MatchingMento />}/>
+              <Route path='/matchingEnd' element={<MatchingEnd />}/>
+              <Route path='/matchingApplication' element={<MatchingApplication />}/>
 
-                <Route path='/mypage' element={<My />} />
-                <Route path='/profile/edit' element={<MyProfileEdit />} />
-            </Routes>
-        </Router>
-      </ProjectProvider>
+              <Route path='/mypage' element={<My />} />
+              <Route path='/profile/edit' element={<MyProfileEdit />} />
+          </Routes>
+      </Router>
     )
 }
 
