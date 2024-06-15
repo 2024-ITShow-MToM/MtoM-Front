bc5a7f462b21cfdd9236c522f73484d5d77312a0
boram065 <boram4784@gmail.com>
2024-06-16 01:12:17 +0900

    Feat: 인터뷰 작성

--- src/pages/InterviewContent.js
+++ src/pages/InterviewContent.js
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useContext } from "react";
+import { InterviewContext } from "../components/home/InterviewProvider";
+
 import Header from "../components/common/Header";
 import styles from "../styles/interview/InterviewContent.module.css"
 import QandAQuestionProfile from "../components/q&a/question/Q&AQuestionProfile";
@@ -7,6 +9,14 @@ import PostTitle from "../components/q&a/question/PostTitle"
 import PostInfo from "../components/q&a/question/PostInfo"
 
 function InterviewContent() {
+    // postTitle: title
+    // postInfo: data
+    const { title, info } = useContext(InterviewContext);
+    const data = {
+        createdAt: '2024.06.14',
+        view: 3
+    };
+
     return(
         <div>
             <Header text="인터뷰"/>
@@ -16,13 +26,17 @@ function InterviewContent() {
             </div>
             <div className={styles2["home-container"]}>
                 <div className={styles['post-container']}>
-                    <PostTitle />
-                    <PostInfo />
+                    <PostTitle title={title} />
+                    <PostInfo data={data} />
                 </div>
                 <div className={styles["content-container"]}>
-                    <p>안녕하세요</p>
-                    <img src="/images/example.png" alt="참고자료"/>
-                    <p>안녕하세요</p>
+                    {
+                        info && Array.isArray(info) && info.map((item, index) => (
+                            <p key={index}>{item}</p>
+                        ))
+                    }
+                    {/* <img src="/images/example.png" alt="참고자료"/>
+                    <p>안녕하세요</p> */}
                 </div>
             </div>
         </div>
